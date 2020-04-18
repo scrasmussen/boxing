780e3096edd551749c243eb1ef3acea8f1fbc765
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-04-18 22:04:28 +0100

    done with mma !

--- spoiler_free.py
+++ spoiler_free.py
@@ -6,7 +6,7 @@ import requests
 
 print("start")
 
-def check_columns(rows):
+def check_boxing_columns(rows):
     total = 0
     for row in rows:
         switch =  {
@@ -21,7 +21,28 @@ def check_columns(rows):
         }
         if (switch.get(row,False)):
             total += 1
-    if (total > 5):
+    if (total > 7):
+        return True
+    return False
+
+def check_mma_columns(rows):
+    total = 0
+    for row in rows:
+        switch =  {
+            'Res.': True,
+            'Record': True,
+            'Opponent': True,
+            'Method':    True,
+            'Event': True,
+            'Date': True,
+            'Round': True,
+            'Time': True,
+            'Location': True
+        }
+        if (switch.get(row,False)):
+            total += 1
+
+    if (total > 8):
         return True
     return False
 
@@ -30,24 +51,30 @@ url = 'https://en.wikipedia.org/wiki/'
 fighter = 'Floyd_Mayweather_Jr.'
 fighter = 'Lawrence_Okolie'
 # fighter = 'Murat_Gassiev'
-# fighter = 'Alexander_Gustafsson'
+fighter = 'Alexander_Gustafsson'
 
 url += fighter
 
-# req = open('full_floyd_wiki.html','r').read()
+# req = open('floyd_wiki.html','r').read()
 req = requests.get(url).text
 
 soup = BeautifulSoup(req, 'lxml')
 result = soup.find_all('table', {'class': 'wikitable'})
 
 for res in result:
-    # print(res)
     tr = res.find('tr')
     td = tr.find_all('th')
     rows = [i.text.rstrip() for i in td]
-    if (check_columns(rows)):
+    # print(rows)
+    if (check_boxing_columns(rows)):
         table = pd.read_html(res.prettify())[0]
         q = table[['No.', 'Opponent', 'Date']]
+    elif (check_mma_columns(rows)):
+        table = pd.read_html(res.prettify())[0]
+        q = table[['Opponent', 'Event', 'Date']].copy().dropna()
+        q.insert(0, '', list(range(len(q.index),0,-1)))
+        new_date = pd.to_datetime(q['Date']).dt.strftime('%m.%d.%Y')
+        q.loc[:,'Date'] = new_date
 
 print(tabulate(q,headers='keys',tablefmt='psql',showindex=False))
 print("Fin")
