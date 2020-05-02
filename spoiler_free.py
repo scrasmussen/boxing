7ccda66e397609d29b8c95a3ca8db461ed790d65
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-05-02 22:29:48 +0100

    up up up

--- spoiler_free.py
+++ spoiler_free.py
@@ -6,6 +6,7 @@ import requests
 # -- Boxers --
 fighter = 'Floyd_Mayweather_Jr.'
 fighter = 'Murat_Gassiev'                    #  eventually; on DAZN
+fighter = 'Jarrett_Hurd'                     #  12.01.2018
 fighter = 'Oleksandr_Usyk'                   #  11.10.2018
 fighter = 'Canelo_Álvarez'                   #  09.15.2018
 fighter = 'Rom%C3%A1n_Gonz%C3%A1lez_(boxer)' #  09.15.2018  # chocolatito
@@ -17,32 +18,37 @@ fighter = 'Daniel_Dubois_(boxer)'            #  06.23.2018
 fighter = 'Errol_Spence_Jr.'                 #  06.16.2018
 fighter = 'Josh_Warrington'                  #  05.19.2018
 fighter = 'Vasyl_Lomachenko'                 #  05.12.2018
+fighter = 'Gennady_Golovkin'                 #  05.05.2018
+fighter = 'Amir_Khan_(boxer)'                #  04.21.2018 vs
+fighter = 'Jermall_Charlo'                   #  04.21.2018 vs Centeno
+fighter = 'Adrian_Broner'                    #  04.21.2018 vs Vargas
+                                             #       Davis vs Cuellar
+
+ # https://www.forbes.com/sites/brianmazique/2018/04/02/boxing-schedule-april-2018-tv-and-stream-info-results-reported-earnings-and-viewership/#46c7737159f2
 
 
 # -- MMA --
 fighter = 'Alexander_Gustafsson'    # Retired
  # leg tied up but still jumps to knee opponent!
 fighter = 'Felicia_Spencer'         #  02.29.2020  UFC Fight Night
-fighter = 'Diego_Sanchez'           #  02.15.2020 UFC Fight Night
+fighter = 'Diego_Sanchez'           #  02.15.2020  UFC Fight Night
 fighter = 'Arnold_Allen_(fighter)'  #  01.14.2020  UFC Fight Night
-fighter = 'Frankie_Edgar'           #  12.21.2019 UFC Fight Night
+fighter = 'Frankie_Edgar'           #  12.21.2019  UFC Fight Night
 fighter = 'Colby_Covington'         #  12.14.2019  UFC 245
 fighter = 'Marlon_Moraes'           #  12.14.2019  UFC 245
 fighter = 'Petr_Yan'                #  12.14.2019  UFC 245
-fighter = 'Song_Yadong'             #  12.07.2019
+fighter = 'Song_Yadong'             #  12.07.2019  UFC on ESPN
 fighter = 'Jorge_Masvidal'          #  11.02.2019  UFC 244
+                                    #  UFC Fight Night: Andrade vs. Zhang 13:45
 
 # -- Currently Watching ---
-# Boxing May 2018 || MMA August 2019
+# Boxing April 2018 || MMA August 2019
 fighter = 'Lyoto_Machida'           # Bellator, past @ UFC 67 02.03.2007
 fighter = 'Andre_Ward'
 fighter = 'Buster_Douglas'
 fighter = 'Mike_Tyson'
 fighter = 'Bernard_Hopkins'
 fighter = ''
-
-
-
 
 
 def check_boxing_columns(rows):
