21bf52860d6d35c371035db9345e99ab4b2a4fa8
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-05-10 14:44:18 +0100

    will display spoiler free main event and prelim fight card

--- fight_card.py
+++ fight_card.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import random
 from sys import exit
 from bs4 import BeautifulSoup
 from tabulate import tabulate
@@ -8,8 +9,6 @@ from tabulate import tabulate
 
 event = ''
 event = 'UFC_Fight_Night:_Hermansson_vs._Cannonier'
-
-
 
 def check_boxing_columns(rows):
     total = 0
@@ -74,39 +73,40 @@ soup = BeautifulSoup(req, 'lxml')
 result = soup.find('table', {'class': 'toccolours'})
 td = result.find_all('tr')
 rows = [i.text.rstrip() for i in td]   # dont need this right now
-
 table = pd.read_html(result.prettify())[0]
+
+# Fix column names
 column_names = [i[1] for i in table.columns]
+column_names[1] = 'Fighter 1'
+column_names[2] = 'vs.'
+column_names[3] = 'Fighter 2'
 table.columns = column_names
+
+# Remove unwanted columns and change to 'vs.'
+table = table.drop(['Method','Round','Time','Notes'], axis=1)
+table['vs.'] = 'vs.'
+
+# Randomly choose which fighter is where
+for index, row in table.iterrows():
+    x = row['Fighter 1']
+    y = row['Fighter 2']
+    if (random.choice([True,False])):
+        row['Fighter 1'] = x
+        row['Fighter 2'] = y
+    else:
+        row['Fighter 1'] = y
+        row['Fighter 2'] = x
+
 
 # split tables
 has_prelim = table['Weight class'].str.contains("Preliminary")
 prelim_row_i = list(has_prelim).index(True)
 maincard = table.iloc[:prelim_row_i,:]
 prelims  = table.iloc[prelim_row_i+1:,:]
-# q = pd.read_html(result.prettify())
-exit()
 
- # this will read too much stuff in || pd.read_html(soup.prettify())
-
-for res in result:
-    tr = res.find('tr')
-    td = tr.find_all('th')
-    rows = [i.text.rstrip() for i in td]
-    # print(rows)
-    if (check_boxing_columns(rows)):
-        table = pd.read_html(res.prettify())[0]
-        q = table[['No.', 'Opponent', 'Date']]
-    elif (check_mma_columns(rows)):
-        table = pd.read_html(res.prettify())[0]
-        q = table[['Opponent', 'Event', 'Date']].copy().dropna()
-        # q.insert(0, '', list(range(len(q.index),0,-1)))
-        # new_date = pd.to_datetime(q['Date']).dt.strftime('%m.%d.%Y')
-        # q.loc[:,'Date'] = new_date
-        # shorten_lambda = lambda x: x[0:x.find(':')] if (x.find(':')>0) else x
-        # q.loc[:,'Event'] = q[['Event']].applymap(shorten_lambda)
-
-
-# print(tabulate([[event.replace('_',' ')]], tablefmt='psql'))
-# print(tabulate(q,headers='keys',tablefmt='psql',showindex=False))
+print(tabulate([[event.replace('_',' ')]], tablefmt='psql'))
+print(tabulate([["Maincard"]], tablefmt='psql'))
+print(tabulate(maincard,headers='keys',tablefmt='psql',showindex=False))
+print(tabulate([["Prelims"]], tablefmt='psql'))
+print(tabulate(prelims,headers='keys',tablefmt='psql',showindex=False))
 print("Fin")
